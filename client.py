d4d70cca2a90cf9e7997fd3596f229651af84de8
Abraham Garcia <abrahamgdc@hotmail.com>
2022-06-09 23:58:53 -0500

    Listas funciones para hstcp
    Los sockets ya funcionan de forma correcta, ya se recibe la misma cantidad de
    informacion que se envia, ahora lo que hay que hacer es aplicar los incrementos
    y decrementos del cwnd segun corresponde y segun marca el propio algoritmo

--- client.py
+++ client.py
@@ -9,22 +9,28 @@ server_address = ("localhost", 10000)
 print("connecting to {} port {}".format(*server_address))
 sock.connect(server_address)
 l = [i for i in range(1000)]
+# print(len(pickle.dumps(l)))
 try:
 
     # Send data
-    message = b"This is the message.  It will be repeated."
+    # message = b"This is the message.  It will be repeated."
+    message = pickle.dumps(l)
+    print(len(message))
     # print("sending {!r}".format(message))
-    print(f"sending {pickle.dumps(l)}")
-    sock.sendall(pickle.dumps(l))
+    # print(f"sending {pickle.dumps(l)}")
+    sock.sendall(message)
 
     # Look for the response
     amount_received = 0
-    amount_expected = len(pickle.dumps(l))
+    amount_expected = len(message)
+    # print(f"expected: {amount_expected}")
 
     while amount_received < amount_expected:
         data = sock.recv(16)
         amount_received += len(data)
+        print(len(data))
         print("received {!r}".format(data))
+    print(f"final {amount_expected}")
 
 finally:
     print("closing socket")
--- server.py
+++ server.py
@@ -1,38 +1,95 @@
+import math
 import pickle
 import socket
 
 # Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+hw = 80
+lw = 13
+r = 0.1
+cwnd_initial = 16
 
-# Bind the socket to the port
-server_address = ("localhost", 10000)
-print("starting up on {} port {}".format(*server_address))
-sock.bind(server_address)
+# This is for TCP
+def increase_cwnd(cwnd):
+    result = cwnd + (1 / cwnd)
+    return result
 
-# Listen for incoming connections
-sock.listen(1)
-final = pickle.dumps("")
-while True:
-    # Wait for a connection
-    print("waiting for a connection")
-    connection, client_address = sock.accept()
-    try:
-        print("connection from", client_address)
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            # print("received {!r}".format(pickle.dumps(data)))
-            if data:
-                print("received {}".format(pickle.dumps(data)))
-                print("sending data back to the client")
-                final += data
-                connection.sendall(pickle.dumps(data))
-            else:
-                print("no data from", client_address)
-                break
+def decrease_cwnd(cwnd):
+    cwnd = cwnd / 2
+    sst = cwnd
+    return (cwnd, sst)
 
-    finally:
-        # Clean up the connection
-        print(f"hello {pickle.loads(final)}")
-        connection.close()
+
+def timeout(cwnd):
+    sst = max((cwnd / 2), cwnd_initial)
+    cwnd = cwnd_initial
+    return (cwnd, sst)
+
+
+# This is for HSTCP
+def increase_cwnd_hs(cwnd):
+    g_beta = decrease_cwnd_hs(cwnd)
+    numerator = pow(cwnd, 2) * (0.078 / 1.2) * 2 * (g_beta / (2 - g_beta)) + 0.5
+    return numerator
+
+
+def decrease_cwnd_hs(cwnd):
+    result = (r - 0.5) * (
+        ((math.log10(cwnd)) - (math.log10(lw))) / ((math.log10(hw)) - (math.log10(lw)))
+    ) + 0.5
+    return result
+
+
+def loss_hs(cwnd):
+    cwnd = (1 - decrease_cwnd_hs(cwnd)) * cwnd
+    sst = cwnd
+    return (cwnd, sst)
+
+
+def timeout_hs(cwnd):
+    cwnd = (1 - decrease_cwnd_hs(cwnd)) * cwnd
+    sst = max(cwnd, cwnd_initial)
+    cwnd = cwnd_initial
+    return (cwnd, sst)
+
+
+def main():
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to the port
+    server_address = ("localhost", 10000)
+    print("starting up on {} port {}".format(*server_address))
+    sock.bind(server_address)
+
+    # Listen for incoming connections
+    sock.listen(1)
+    amount_rec = 0
+    while True:
+        # Wait for a connection
+        print("waiting for a connection")
+        connection, client_address = sock.accept()
+        try:
+            print("connection from", client_address)
+            # Receive the data in small chunks and retransmit it
+            cwnd = cwnd_initial
+            while True:
+                data = connection.recv(cwnd)
+                # print(len(data))
+                # print("received {!r}".format(pickle.dumps(data)))
+                if data:
+                    amount_rec += len(data)
+                    # print("received {}".format(pickle.dumps(data)))
+                    print(len(data))
+                    print("sending data back to the client")
+                    connection.sendall(pickle.dumps(data))
+                else:
+                    print("no data from", client_address)
+                    break
+        finally:
+            # Clean up the connection
+            print(f"final: {amount_rec}")
+            connection.close()
+
+
+if __name__ == "__main__":
+    main()
